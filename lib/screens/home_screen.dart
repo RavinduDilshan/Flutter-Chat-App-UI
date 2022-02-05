83e290692d5f68c77312bb5d3d96ce688c2b3448
RavinduDilshan <dilshan.ravidu@gmail.com>
2022-02-05 16:36:53 +0530

    app bar designed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        title: Text(
+          'Chats',
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search))
+        ],
+      ),
+    );
   }
 }
